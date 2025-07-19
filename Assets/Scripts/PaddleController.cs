cb242adf2c5617cecc675b2183d487d922ed2f54
datafedai <datafedai@gmail.com>
2025-07-19 13:58:58 -0400

    ballcontroller removed

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -19,12 +19,15 @@ public class PaddleController : MonoBehaviour
     public int moveSpeed;
     private float posX;
     private float posY;
+    private float posZ;
+    private float wall;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         moveSpeed = 20;
+        wall = 13f;
     }
 
     // Update is called once per frame
@@ -42,12 +45,14 @@ public class PaddleController : MonoBehaviour
             Debug.Log("right arrow pressed");
         }
         */
-        
+
 
         // move paddle left or right
         posX = transform.position.x;
         posY = transform.position.y;
+        posZ = transform.position.z;
         //Debug.Log("posX: " + posX);
+        /*
         if (posX < -12f)
         {
             transform.position = new Vector3(-11.9f, posY, 0);
@@ -56,9 +61,14 @@ public class PaddleController : MonoBehaviour
         {
             transform.position = new Vector3(11.9f, posY, 0);
         }
+
         transform.Translate(moveValue.x * Time.deltaTime * moveSpeed, 0, 0);
+        */
 
-
-
+        float moveDistance = moveValue.x * Time.deltaTime * moveSpeed;
+        Vector3 newPos = transform.position + new Vector3(moveDistance, 0, 0);
+        newPos.x = Mathf.Clamp(newPos.x, -wall, wall);
+        transform.position = newPos;        
+        
     }
 }
