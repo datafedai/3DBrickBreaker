5599668fcadf90c10bed3ce818df727d360878b7
datafedai <datafedai@gmail.com>
2025-08-09 22:16:36 -0400

    score script

---
+++ Assets/Scripts/HighScores.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System;
+
+
+[Serializable]
+public class HighScores : MonoBehaviour
+{
+
+
+
+
+    public string playerName;
+    public int score;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
