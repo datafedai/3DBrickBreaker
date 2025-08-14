1399d3cdd9e1855b2d037e4f36078f8c15ec64fc
datafedai <datafedai@gmail.com>
2025-08-14 15:58:05 -0400

    brick animation added

--- Assets/Scripts/brick.cs
+++ Assets/Scripts/brick.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 public class brick : MonoBehaviour
 {
 
+    private Animator anim;
     void OnEnable()
     {
         // Subscribe to the ball's BrickDestroyed event
@@ -22,14 +23,38 @@ public class brick : MonoBehaviour
         // For example, you can destroy the brick or change its state
         Debug.Log("Brick hit by ball: " + gameObject.name);
         Rigidbody brickRB = brickObject.GetComponent<Rigidbody>();
-        brickRB.isKinematic = false; // Make sure the brick can be affected by physics
-        //brickRB.AddForce(Vector3.up * 5f, ForceMode.Impulse); //
-        brickRB.useGravity = true;
-        brickRB.rotation = Quaternion.Euler(90, 45, 0);
+        //brickRB.isKinematic = false; // Make sure the brick can be affected by physics
+        //brickRB.useGravity = true;
+        //brickRB.rotation = Quaternion.Euler(90, 45, 0);
+
+        anim = brickObject.GetComponent<Animator>();
+        anim.Play("brickAnimation");
     }
 
 
+    private void FindAndDestoryBrickClone()
+    {
+        // if fallen bricks are under paddle position, destroy
+        GameObject[] foundObject = GameObject.FindGameObjectsWithTag("BrickClone");
+        if (foundObject != null)
+        {
+            foreach (GameObject each in foundObject)
+            {
+                //Debug.Log("Found object: " + foundObject[i].name + " : " + i);
+                //Debug.Log("Found the object: " + foundObject[0].name);
+                if (each.transform.position.y < -4f)
+                {
+                    Destroy(each);
+                }
+            }
 
+        }
+        else
+        {
+            Debug.Log("Object not found!");
+        }
+
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -41,6 +66,7 @@ public class brick : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FindAndDestoryBrickClone();
 
     }
 }
